2c15d9e751518966c1c22c71fd6e3dbadc9a705a
Lanazen <svetatressla@gmail.com>
2017-11-28 01:54:08 +0700

    Function for random array

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/* var titleOffer = [
+/* var TITLEOFFER = [
   'Большая уютная квартира',
   'Маленькая неуютная квартира',
   'Огромный прекрасный дворец',
@@ -11,19 +11,19 @@
   'Неуютное бунгало по колено в воде'
 ];
 
-var typeOffer = [
+var TYPEOFFER = [
   'flat',
   'house',
   'bungalo'
 ];
 
-var timeOffer = [
+var TIMEOFFER = [
   '12:00',
   '13:00',
   '14:00'
 ];
 
-var featuresOffer = [
+var FEATURESOFFER = [
   'wifi',
   'dishwasher',
   'parking',
@@ -32,32 +32,41 @@ var featuresOffer = [
   'conditioner'
 ];
 
-// Нахождение случайного числа
+// Нахождение случайного числа, max не включен в диапазон, поэтому прибавляем 1
 var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+// Создание массива случайной длины
+var getRandomArray = function () {
+  var randomFeature = [];
+  for (var i = 0; i < getRandomNumber(1, FEATURESOFFER.length - 1); i++) {
+    randomFeature[i] = FEATURESOFFER[getRandomNumber(0, FEATURESOFFER.length - 1)];
+  }
+  return randomFeature;
 };
 
 // Создание массива объектов
 var generateOfferList = function () {
   var offer = [];
   for (var i = 0; i < 8; i++) {
-    var locationX = getRandomNumber(300, 900) + 40 / 2;
-    var locationY = getRandomNumber(100, 500) + 40;
+    var locationX = getRandomNumber(300, 900);
+    var locationY = getRandomNumber(100, 500);
     offer[i] = {
       'author': {
         'avatar': 'img/avatars/user0' + (i + 1) + '.png'
       },
 
       'offer': {
-        'title': titleOffer[i],
+        'title': TITLEOFFER[i],
         'address': locationX + ', ' + locationY,
         'price': getRandomNumber(1000, 1000000),
-        'type': typeOffer[getRandomNumber(0, 2)],
+        'type': TYPEOFFER[getRandomNumber(0, TYPEOFFER.length - 1)],
         'rooms': getRandomNumber(1, 5),
         'guests': getRandomNumber(1, 5),
-        'checkin': timeOffer[getRandomNumber(0, 2)],
-        'checkout': timeOffer[getRandomNumber(0, 2)],
-        'features': '',
+        'checkin': TIMEOFFER[getRandomNumber(0, TIMEOFFER.length - 1)],
+        'checkout': TIMEOFFER[getRandomNumber(0, TIMEOFFER.length - 1)],
+        'features': getRandomArray(),
         'description': '',
         'photos': []
       },
