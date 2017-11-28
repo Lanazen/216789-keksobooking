02f13223a1d0cc91bbdf303c40d9d98b5f81ac7f
Lanazen <svetatressla@gmail.com>
2017-11-28 16:19:42 +0700

    Finished function for random and unique elements in array

--- js/map.js
+++ js/map.js
@@ -1,6 +1,7 @@
 'use strict';
 
-/* var TITLEOFFER = [
+/*
+var TITLE_OFFER = [
   'Большая уютная квартира',
   'Маленькая неуютная квартира',
   'Огромный прекрасный дворец',
@@ -11,19 +12,19 @@
   'Неуютное бунгало по колено в воде'
 ];
 
-var TYPEOFFER = [
+var TYPE_OFFER = [
   'flat',
   'house',
   'bungalo'
 ];
 
-var TIMEOFFER = [
+var TIME_OFFER = [
   '12:00',
   '13:00',
   '14:00'
 ];
 
-var FEATURESOFFER = [
+var FEATURES_OFFER = [
   'wifi',
   'dishwasher',
   'parking',
@@ -32,42 +33,62 @@ var FEATURESOFFER = [
   'conditioner'
 ];
 
+var LOCATION = {
+  X: {
+    MIN: 300,
+    MAX: 900
+  },
+  Y: {
+    MIN: 100,
+    MAX: 500
+  }
+};
+
+var PINS_AMOUNT = 8;
+var MIN_PRICE = 1000;
+var MAX_PRICE = 1000000;
+var MIN_ROOMS = 1;
+var MAX_ROOMS = 5;
+var MIN_GUESTS = 1;
+var MAX_GUESTS = 5;
+
 // Нахождение случайного числа, max не включен в диапазон, поэтому прибавляем 1
 var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-// Создание массива случайной длины
+// Создание массива случайной длины из уникальных элементов
 var getRandomArray = function () {
-  var randomFeature = [];
-  var featuresOfferCopy = FEATURESOFFER.slice();
-  for (var i = 0; i < getRandomNumber(1, FEATURESOFFER.length + 1); i++) {
-    randomFeature[i] = featuresOfferCopy.splice(getRandomNumber(0, FEATURESOFFER.length - 1), 1);
-    var newArray = featuresOfferCopy.concat(randomFeature[i]);
+  var resultFeatures = [];
+  var featuresOfferCopy = FEATURES_OFFER.slice();
+  var randomArrayLength = getRandomNumber(0, featuresOfferCopy.length - 1);
+  for (var i = 0; i <= randomArrayLength; i++) {
+    var indexRandom = getRandomNumber(0, featuresOfferCopy.length - 1);
+    resultFeatures[i] = featuresOfferCopy.splice(indexRandom, 1).toString();
   }
-  return newArray;
+  return resultFeatures;
 };
 
 // Создание массива объектов
 var generateOfferList = function () {
   var offer = [];
-  for (var i = 0; i < 8; i++) {
-    var locationX = getRandomNumber(300, 900);
-    var locationY = getRandomNumber(100, 500);
+  for (var i = 0; i < PINS_AMOUNT; i++) {
+    var locationX = getRandomNumber(LOCATION.X.MIN, LOCATION.X.MAX);
+    var locationY = getRandomNumber(LOCATION.Y.MIN, LOCATION.Y.MAX);
     offer[i] = {
       'author': {
         'avatar': 'img/avatars/user0' + (i + 1) + '.png'
       },
 
       'offer': {
-        'title': TITLEOFFER[i],
+        'title': TITLE_OFFER[i],
         'address': locationX + ', ' + locationY,
-        'price': getRandomNumber(1000, 1000000),
-        'type': TYPEOFFER[getRandomNumber(0, TYPEOFFER.length - 1)],
-        'rooms': getRandomNumber(1, 5),
-        'guests': getRandomNumber(1, 5),
-        'checkin': TIMEOFFER[getRandomNumber(0, TIMEOFFER.length - 1)],
-        'checkout': TIMEOFFER[getRandomNumber(0, TIMEOFFER.length - 1)],
+        'price': getRandomNumber(MIN_PRICE, MAX_PRICE),
+        'type': TYPE_OFFER[getRandomNumber(0, TYPE_OFFER.length - 1)],
+        'rooms': getRandomNumber(MIN_ROOMS, MAX_ROOMS),
+        'guests': getRandomNumber(MIN_GUESTS, MAX_GUESTS),
+        'checkin': TIME_OFFER[getRandomNumber(0, TIME_OFFER.length - 1)],
+        'checkout': TIME_OFFER[getRandomNumber(0, TIME_OFFER.length - 1)],
         'features': getRandomArray(),
         'description': '',
         'photos': []
